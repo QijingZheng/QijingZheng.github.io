0ee837b9a5ef16cc16a9236c84eada372346e8a8
QijingZheng <zqj.kaka@gmail.com>
2022-09-22 22:14:19 +0800

    change permission to 644 for /assets/img/me/gallery/20171118.jpg

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.importScripts('/~zqj/assets/js/data/cache-list.js');
+self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20220922.2201';
+var cacheName = 'chirpy-20220922.2213';
 
 
 function isExcluded(url) {
